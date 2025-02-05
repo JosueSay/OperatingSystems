7807959fe3a841f180cbc009cea2563d95911dfe
JosueSay <josuesay770@gmail.com>
2025-02-05 17:32:21 -0500

    parte 1 completa

---
+++ part1b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+  int f = fork();
+
+  if (f == 0){
+    execl("./part1a", "part1a", (char*)NULL); // Se adjunta un arg, para evitar los warings en consola
+  }else{
+    printf("%d\n", (int)getpid());  
+    execl("./part1a", "part1a", (char*)NULL);
+  }
+
+  return 0;
+}
