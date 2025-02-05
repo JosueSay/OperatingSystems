b724e505174a1560c4b72ff349a396dda55d41ad
JosueSay <josuesay770@gmail.com>
2025-02-05 17:37:20 -0500

    cambio nombre ejecutable

--- part1b.c
+++ part1b.c
@@ -4,10 +4,10 @@ int main(){
   int f = fork();
 
   if (f == 0){
-    execl("./part1a", "part1a", (char*)NULL); // Se adjunta un arg, para evitar los warings en consola
+    execl("./part1a.o", "part1a", (char*)NULL); // Se adjunta un arg, para evitar los warings en consola
   }else{
     printf("%d\n", (int)getpid());  
-    execl("./part1a", "part1a", (char*)NULL);
+    execl("./part1a.o", "part1a", (char*)NULL);
   }
 
   return 0;
